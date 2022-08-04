7d2961410b5c399dd0341b4263e6e04a06d18b18
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2022-08-04 12:02:35 +0530

    LEVEL 1

--- calculator.py
+++ calculator.py
@@ -1,33 +1,35 @@
 import math
-def fact(n):
+
+def fact(n):                                         # factorial
     f=1;
     s=n
     for i in range(1,n):
         f=f*s;
         s=s-1;
     return(f)
+
 def calr(n): 
-    if n == '1':                     #sum
+    if n == '1':                                      #sum
         print("\tAddition")
         a=int(input("Enter First Number:"))
         b=int(input("Enter Second Number:")) 
         print("\n",a,"+",b,"=",a+b)
-    elif n == '2':                   #difference
+    elif n == '2':                                   #difference
         print("\tSubtraction")
         a=int(input("Enter First Number:"))
         b=int(input("Enter Second Number:")) 
         print("\n",a,"-",b,"=",a-b) 
-    elif n == '3':                   #multiplication 
+    elif n == '3':                                   #multiplication 
         print("\tMultiplication")
         a=int(input("Enter First Number:"))
         b=int(input("Enter Second Number:")) 
         print("\n",a,"*",b,"=",a*b)
-    elif n == '4':                   #division     
+    elif n == '4':                                   #division     
         print("\tDivision")    
         a=int(input("Enter First Number:"))
         b=int(input("Enter Second Number:")) 
         print("\n",a,"/",b,"=",float(a/b))
-    elif n == '5':                   #x^n
+    elif n == '5':                                   #x^n
         print("\tPower")
         a=int(input("Enter Number:"))
         b=int(input("Enter exponent:"))
@@ -37,34 +39,35 @@ def calr(n):
             x=x*a;
             b=b-1;
         print("\n",a,"^",c,"=",x)
-    elif n == '6':                   #logarithm
+    elif n == '6':                                  #logarithm
         print("\tLogarithm")
         a=int(input("Enter Number:"))
         b=int(input("Enter base:"))
         print("Result is:",math.log(a,b))
-    elif n == '7':                   #natural logarithm
+    elif n == '7':                                  #natural logarithm
         print("\tNatural Logarithm")
         a=int(input("Enter Number:"))
         print("Result is:",math.log(a))
-    elif n == '8':                   #nCr
+    elif n == '8':                                  #nCr
         print("\tCombination(nCr)")
         a=int(input("Enter Number(n):"))
         b=int(input("Enter Number(r):"))
         c=a-b
         print("Result :",fact(a)/(fact(b)*fact(c)))
-    elif n == '9':                   #nPr
+    elif n == '9':                                 #nPr
         print("\tPermutation(nPr)")
         a=int(input("Enter Number(n):"))
         b=int(input("Enter Number(r):"))
         c=a-b
         print("Result :",fact(a)/fact(c))
-    elif n == '10':                  #e^x
+    elif n == '10':                                #e^x
         print("\tExponential")
         a=int(input("Enter Number:"))
         print("Result is:",math.exp(a))
     else:
         print("Some error occured,Try again")
-        
+ 
+
 # main program    
 print("""
        Calculator
